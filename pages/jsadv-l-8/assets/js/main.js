960da7c1e41d013a2312c8629015d227e8207279
Andriy Holomsha <andrushah@gmail.com>
2021-10-28 19:19:49 +0300

    jsadv-l-8 done

---
+++ pages/jsadv-l-8/assets/js/main.js
@@ -0,0 +1,103 @@
+// Завдання 5 пошук неправильних слів
+let wwTrext = document.getElementById('wwTrext'); //Доступ до поля вводу нових слів
+let wrongWords = document.querySelector('.wrong-words'); // Достуд до елемента куди будуть записуватись нові слова
+let textArea = document.querySelector('#textarea'); // Доступ до текстового поля що буде фільтруватися
+let addBtn = document.querySelector('#addBtn'); // Доступ до кнопки добавити
+let resetBtn = document.querySelector('#resetBtn'); // Кнопка RESET
+let censorBtn = document.querySelector('#censoreBtn'); // Кнопка Censor
+let ivanidField = false; // змінна для валідації 
+let ivanidTextArea = false; // змінна для валідації 
+// ф-ція отримання списку неправильних слів
+function getWrongWords() {
+    let str = wrongWords.textContent;
+    let word = '';
+    let wordsList = [];
+    if (str.length) {
+        for (let i = 0; i < str.length; i++) {
+            // перевірка чи не кінеці слова
+            if (str.charAt(i) != ',' && str.charAt(i + 1) != '&nbsp;') {
+                word += str.charAt(i);
+            }
+            else {
+                i++; // пропускаємо наступний пробіл
+                if (wordsList.length) {
+                    // первірка унікальності слова(чи не повторяється)
+                    if (wordsList.includes(word))
+                        word = '';
+                }
+                // якщо не повторяється записуємо в масив
+                if (word != '') {
+                    wordsList.push(word);
+                    word = '';
+                }
+            }
+            // запис останнього слова в масив
+            if (i == str.length - 1) {
+                if (!wordsList.includes(word))
+                    wordsList.push(word);
+            }
+        }
+        return wordsList; // повернення заповненого масиву
+    }
+    else {
+        console.log('Wrong words list is empty');
+        return wordsList; // повернення порожнього масиву
+    }
+}
+// Скидання слів
+function resetWrongWords() {
+    wrongWords.innerHTML = '';
+}
+//додавання дових слів 
+function addWrongWords() {
+    if (wwTrext.value == '') {
+        wwTrext.classList.add('invalide');
+        ivanidField = true;
+    }
+    else if (!wrongWords.textContent.length && wwTrext.value != '') {
+        wrongWords.innerHTML += wwTrext.value;
+        wwTrext.classList.remove('invalide');
+        wwTrext.value = '';
+    }
+    else {
+        wrongWords.innerHTML += `, ${wwTrext.value}`;
+        wwTrext.classList.remove('invalide');
+        wwTrext.value = '';
+    }
+}
+// перевірка на наявність неправильних слів у введеному полі текст(Censore)
+function checkWords() {
+    let arr = getWrongWords();
+    let text = textArea.value;
+    if (text.length && arr.length) {
+        arr.forEach(element => {
+            if (text.includes(element)) {
+                let mark = '*';
+                for (let i = 0; i < text.length; i++) {
+                    text = text.replace(element, mark.repeat(element.length));
+                }
+            }
+        });
+        textArea.value = text; // запис даних в HTML
+        textArea.classList.add('success'); //додавання класу після успішного виконання
+    }
+    else if (!textArea.value.length) {
+        textArea.classList.add('invalide'); //додавання класу після НЕ успішного виконання
+        ivanidTextArea = true; //запус процедури валідаціЇ на подію онінпут цього поля
+    }
+}
+// валідація поля на подію ONINPUT
+wwTrext.addEventListener('input', function () {
+    if (ivanidField) {
+        this.value.length < 1 ? this.classList.add('invalide') : this.classList.remove('invalide');
+        ivanidField = false;
+    }
+});
+// валідація поля на подію ONINPUT
+textArea.addEventListener('input', function () {
+    if (ivanidTextArea) {
+        this.value.length < 1 ? this.classList.add('invalide') : this.classList.remove('invalide');
+        ivanidTextArea = false;
+    }
+    this.classList.remove('success');
+});
