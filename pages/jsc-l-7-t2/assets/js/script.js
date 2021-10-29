1c93c4059e2409cce6033f5d2bc88fae5affca9b
Andriy Holomsha <andrushah@gmail.com>
2021-10-29 15:58:37 +0300

    start adding git links

--- pages/jsc-l-7-t2/assets/js/script.js
+++ pages/jsc-l-7-t2/assets/js/script.js
@@ -1,20 +1,25 @@
 window.addEventListener('keydown', function (event) {
     let keyboard = document.querySelectorAll('.row>li');
     let key = event.key;
-    let keyCode = event.keyCode;
     let screen = document.querySelector('.screen');
+    let unsupportKeys = ["ArrowLeft", "ArrowUp", "ArrowRight", "ArrowDown", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12"]; //set of unsupported keys
+    for (i = 0; i < unsupportKeys.length; i++) {
+        if (event.key == unsupportKeys[i]) {
+            return;
+        }
+    }
+
     if (event.key == "Tab") {
         event.preventDefault();
         screen.innerHTML += '&emsp;'; // 4 spaces
-    }else if(key == ' '){
+    } else if (key == ' ') {
         event.preventDefault();
-        screen.innerHTML+=key;
-    } 
-    else if (event.key == 'Backspace') {
+        screen.innerHTML += key;
+    } else if (event.key == 'Backspace') {
         screen.innerHTML = screen.innerHTML.substring(0, screen.innerHTML.length - 1);
     } else if (event.key == 'Enter') {
         screen.innerHTML += '<br>'
-    } else if (event.key== 'Shift' || event.key == 'MetaShift' || event.key == 'AltShift' || event.key == 'Alt' || event.key == 'Control' || event.key == 'Meta' || event.key == 'CapsLock') {
+    } else if (event.key == 'Shift' || event.key == 'MetaShift' || event.key == 'AltShift' || event.key == 'Alt' || event.key == 'Control' || event.key == 'Meta' || event.key == 'CapsLock') {
 
     } else {
         screen.innerHTML += event.key;
